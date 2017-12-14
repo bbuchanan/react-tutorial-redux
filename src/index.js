fec76ff66041f4847a8136de2e27147e6db3e3df
Bill Buchanan <bbuchanan@gmail.com>
2017-12-14 17:29:01 -0500

    refactored the reducers into separate reducers for counter and result

--- src/index.js
+++ src/index.js
@@ -4,11 +4,16 @@ import { Provider } from 'react-redux'
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { createStore } from 'redux'
-import reducer from './store/reducer'
+import { createStore, combineReducers } from 'redux'
+import counterReducer from './store/reducers/counter'
+import resultReducer from './store/reducers/result'
 
+const rootReducer = combineReducers({
+  ctr: counterReducer,
+  res: resultReducer
+})
 
-const store = createStore(reducer)
+const store = createStore(rootReducer)
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
